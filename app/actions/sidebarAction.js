59f7cbac49c4b0bbf8283754163df6e32265ebc6
Tushar  Borole <tushariscoolster@gmail.com>
2018-10-24 21:02:03 -0700

    Refactored code

--- app/actions/sidebarAction.js
+++ app/actions/sidebarAction.js
@@ -12,30 +12,28 @@ const storage = require('electron-json-storage');
 
 storage.setDataPath(os.tmpdir());
 
-console.log(storage.get('credentials'));
-storage.get('credentials', (error, data) => {
-  if (error) throw error;
-  console.log(data);
-  AWS.config.update({
-    region: 'us-east-1',
-    secretAccessKey: data.secretkey,
-    accessKeyId: data.accessKey
-  });
-});
-
 export const requestLogGroups = createAction(REQUEST_LOG_GROUPS);
 export const receiveLogGroups = createAction(RECEIVE_LOG_GROUPS);
 export const receiveLogGroupsFail = createAction(RECEIVE_LOG_GROUPS_FAIL);
 
 export const fetchLogGroups = () => (dispatch, getState) => {
-  const cloudwatchlogs = new AWS.CloudWatchLogs();
   dispatch(requestLogGroups());
   const params = {
     limit: 10
   };
-  cloudwatchlogs.describeLogGroups(params, (err, data) => {
-    if (err) dispatch(receiveLogGroupsFail());
-    // an error occurred
-    else dispatch(receiveLogGroups(data)); // successful response
+  storage.get('credentials', (error, data) => {
+    if (error) throw error;
+    AWS.config.update({
+      region: 'us-east-1',
+      secretAccessKey: data.secretkey,
+      accessKeyId: data.accessKey
+    });
+    const cloudwatchlogs = new AWS.CloudWatchLogs();
+    cloudwatchlogs.describeLogGroups(params, (err, data) => {
+      if (err) dispatch(receiveLogGroupsFail());
+      // an error occurred
+      else dispatch(receiveLogGroups(data)); // successful response
+    });
   });
+
 };
